b3acfe515ba158655410a261e750ce6a7dcdfbf8
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-06-01 11:59:23 +0530

    Question 3 - Remove Duplicates: Solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import striver_dsa_sheet.QuestionThree;
+
 public class Main {
     public static void main(String[] args) {
-
+        QuestionThree questionThree = new QuestionThree();
+        int unique = questionThree.removeDuplicates(new int[]{1, 1, 2});
+        System.out.println(unique);
     }
 }
---
+++ src/striver_dsa_sheet/QuestionThree.java
@@ -0,0 +1,24 @@
+package striver_dsa_sheet;
+
+/**
+ * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that
+ * each unique element appears only once. The relative order of the elements should be kept the same.
+ * Then return the number of unique elements in nums.
+ */
+public class QuestionThree {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+}
