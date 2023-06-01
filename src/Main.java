f26ba829be1b7aef69a75f4854fa408b219c3fcb
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-06-01 11:55:06 +0530

    Question Two - Reverse Array and String: Solution

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
-import striver_dsa_sheet.QuestionOne;
-
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        QuestionOne questionOne = new QuestionOne();
-        int[] response = questionOne.twoSum(new int[]{2, 7, 11, 15}, 9);
-        System.out.println(Arrays.toString(response));
+
     }
 }
---
+++ src/striver_dsa_sheet/QuestionTwo.java
@@ -0,0 +1,32 @@
+package striver_dsa_sheet;
+
+/**
+ * Write a program to reverse an array or string
+ */
+
+public class QuestionTwo {
+
+    public String reverseString(String string) {
+        String newString = "";
+        for (int i = 0; i < string.length(); i++) {
+            newString = string.charAt(i) + newString;
+        }
+        return newString;
+    }
+
+    public int[] reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        return arr;
+    }
+}
