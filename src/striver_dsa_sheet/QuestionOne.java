18385ba256ae8a47e6f85d68d3d0cf42f81edf6e
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-06-01 11:35:08 +0530

    Question One - Two Sum: Solution

--- src/striver_dsa_sheet/QuestionOne.java
+++ src/striver_dsa_sheet/QuestionOne.java
@@ -1,5 +1,29 @@
 package striver_dsa_sheet;
 
-public class QuestionOne {
+import java.util.HashMap;
 
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ */
+
+public class QuestionOne {
+    public int[] twoSum(int[] nums, int target) {
+        int[] output = new int[2];
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                output[0] = hashMap.get(target - nums[i]);
+                output[1] = i;
+                return output;
+            }
+            hashMap.put(nums[i], i);
+        }
+
+        output[0] = -1;
+        output[1] = -1;
+        return output;
+    }
 }
