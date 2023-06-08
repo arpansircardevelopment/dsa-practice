e10d8b622e8e224c87b39f2c99cb74aa4c308a91
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-06-08 07:35:50 +0530

    Missing Numbers Problems Added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import arrays.MissingNumber;
+
 public class Main {
     public static void main(String[] args) {
+        MissingNumber missingNumber = new MissingNumber();
+        missingNumber.sort(new int[]{9,6,4,2,3,5,7,0,1});
     }
 }
---
+++ src/arrays/MissingNumber.java
@@ -0,0 +1,35 @@
+package arrays;
+
+public class MissingNumber {
+
+    public void sort(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] < nums.length && nums[i] != i) {
+                swap(nums, i, nums[i]);
+            } else {
+                i++;
+            }
+        }
+
+        int missingNumber = -1;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j) {
+                missingNumber = j;
+                break;
+            }
+        }
+
+        if (missingNumber == -1) {
+            missingNumber = nums.length;
+        }
+
+        System.out.println("Missing Number: " + missingNumber);
+    }
+
+    public void swap(int[] array, int first, int second) {
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
+}
