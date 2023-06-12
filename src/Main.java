eef885b7cbfe3184897d96427588bf71058abce9
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-06-12 07:46:35 +0530

    First Missing Positive - Solved

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
+import arrays.FirstMissingPositive;
 import arrays.SetMismatch;;import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        SetMismatch setMismatch = new SetMismatch();
-        int[] output = setMismatch.findTheSetMismatch(new int[]{1, 2, 2, 4});
-        System.out.println(Arrays.toString(output));
+        FirstMissingPositive fmp = new FirstMissingPositive();
+        System.out.println(fmp.firstMissingPositive(new int[]{3, 4, -1, 1}));
     }
 }
---
+++ src/arrays/FirstMissingPositive.java
@@ -0,0 +1,30 @@
+package arrays;
+
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i];
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[correct] != nums[i]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i) {
+                return i + 1;
+            }
+        }
+
+        return nums.length;
+    }
+
+    public void swap(int[] array, int first, int second) {
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
+}
