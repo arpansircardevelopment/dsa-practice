7f3546e770ea43edfc2ec4f8a792e773dc623b84
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-06-12 07:04:10 +0530

    Find Duplicates In An Array - Added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
-import arrays.RemoveDuplicatesCyclicSort;
+import arrays.FindDuplicatesInAnArray;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        RemoveDuplicatesCyclicSort removeDuplicates = new RemoveDuplicatesCyclicSort();
-        int abc = removeDuplicates.findDuplicate(new int[]{3, 1, 3, 4, 2});
-        System.out.println(abc);
+        FindDuplicatesInAnArray duplicates = new FindDuplicatesInAnArray();
+        List<Integer> output = duplicates.findDuplicatesInArray(new int[]{4, 3, 2, 7, 8, 2, 3, 1});
+        System.out.println(output);
     }
 }
