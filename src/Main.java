deb41f534bb6355b1cd5cadfa260359bb65238a8
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-06-21 08:15:22 +0530

    Merge Intervals Problem - Added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
-import arrays.MaxSubArray;
-import arrays.MergeSort;
-import arrays.SortAnArrayOfZerosAndOnes;
+import arrays.MergeIntervals;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        MaxSubArray maxSubArray = new MaxSubArray();
-        int sum = maxSubArray.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
-        System.out.println(sum);
+        MergeIntervals mergeIntervals = new MergeIntervals();
+        int[][] result = mergeIntervals.merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}});
+        for (int[] i : result) {
+            System.out.println(Arrays.toString(i));
+        }
     }
 }
