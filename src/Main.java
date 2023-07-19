5adae89d9e1f03c1cd9f25bd55fe708423959a82
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-07-19 07:14:49 +0530

    Rotate Array - Added

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-import arrays.BuyAndSellStock;
-import arrays.NextPermutation;
-import arrays.PascalTriangle;
-import arrays.Search2DArray;
+import arrays.*;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Search2DArray array = new Search2DArray();
-        boolean result = array.searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3);
-        System.out.println(result);
+        RotateArray rotateArray = new RotateArray();
+        rotateArray.rotate(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
     }
 }
---
+++ src/arrays/RotateArray.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public void rotate(int[][] matrix) {
+        int length = matrix.length;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j < length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < (length / 2); j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][length - j - 1];
+                matrix[i][length - j - 1] = temp;
+            }
+        }
+
+        for (int[] array : matrix) {
+            System.out.println(Arrays.toString(array));
+        }
+    }
+}
