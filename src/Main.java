b414b9ae713fe8382269fa65705b80722ce3be89
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-07-19 06:46:37 +0530

    Search 2D Array - Added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import arrays.BuyAndSellStock;
 import arrays.NextPermutation;
 import arrays.PascalTriangle;
+import arrays.Search2DArray;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BuyAndSellStock stock = new BuyAndSellStock();
-        int profit = stock.maxProfit(new int[]{7, 6, 4, 3, 1});
-        System.out.println(profit);
+        Search2DArray array = new Search2DArray();
+        boolean result = array.searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3);
+        System.out.println(result);
     }
 }
---
+++ src/arrays/Search2DArray.java
@@ -0,0 +1,55 @@
+package arrays;
+
+public class Search2DArray {
+
+    // Leetcode solution
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) {
+            return false;
+        }
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int lo = 0;
+        int hi = (n * m) - 1;
+
+        while (lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+
+            if (matrix[mid / m][mid % m] == target) {
+                return true;
+            }
+
+            if (matrix[mid / m][mid % m] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    // GFG solution
+    public boolean searchMatrixGFG(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int i = 0;
+        int j = m - 1;
+
+        while (i < n && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+
+            if (matrix[i][j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return false;
+    }
+}
