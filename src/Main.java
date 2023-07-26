e5fdbeb20b882c72ba1fc16958917f05ce8a0e48
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-07-26 07:30:14 +0530

    Pow(x,n) added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        RotateArray rotateArray = new RotateArray();
-        rotateArray.rotate(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        PowXN powXN = new PowXN();
+       double ans = powXN.myPow(2, 10);
+        System.out.println(ans);
     }
 }
---
+++ src/arrays/PowXN.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class PowXN {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long nn = n;
+        if (n < 0) nn = nn * -1;
+
+        while (nn > 0) {
+            if (nn % 2 == 1) {
+                ans = ans * x;
+                nn--;
+            } else {
+                x = x * x;
+                nn = nn / 2;
+            }
+        }
+
+        if (n < 0) {
+            ans = (double) 1.0 / (double) ans;
+        }
+        return ans;
+    }
+}
