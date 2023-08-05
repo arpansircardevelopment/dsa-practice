324bfca98acaa78f7223c229d9334056039a81ef
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-08-05 07:31:31 +0530

    Largest sum zero subarray

---
+++ src/arrays/LargestSumZeroSubArray.java
@@ -0,0 +1,27 @@
+package arrays;
+
+import java.util.HashMap;
+
+public class LargestSumZeroSubArray {
+    int maxLen(int[] arr, int n) {
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        int maximum = 0;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if (sum == 0) {
+                maximum = i + 1;
+            } else {
+                if (mpp.get(sum) != null) {
+                    maximum = Math.max(maximum, i - mpp.get(sum));
+                } else {
+                    mpp.put(sum, i);
+                }
+            }
+        }
+
+        return maximum;
+    }
+}
