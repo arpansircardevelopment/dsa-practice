02581f02ecb06c34ac4b7648ce9b35aa45e74209
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-08-05 06:55:10 +0530

    Majority Element 2 - Added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import arrays.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        MajorityElement majorityElement = new MajorityElement();
-        int output = majorityElement.majorityElement(new int[]{3,2,3});
-        System.out.println(output);
+        MajorityElement2 majorityElement2 = new MajorityElement2();
+        List<Integer> output = majorityElement2.majorityElement(new int[]{3, 2, 3});
+        System.out.println(output.toString());
     }
 }
---
+++ src/arrays/MajorityElement2.java
@@ -0,0 +1,47 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MajorityElement2 {
+    public List<Integer> majorityElement(int[] nums) {
+        int element1 = Integer.MIN_VALUE, element2 = Integer.MIN_VALUE;
+        int count1 = 0, count2 = 0;
+
+        for (int i : nums) {
+            if (element1 == i) {
+                count1++;
+            } else if (element2 == i) {
+                count2++;
+            } else if (count1 == 0) {
+                element1 = i;
+                count1 = 1;
+            } else if (count2 == 0) {
+                element2 = i;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == element1) count1++;
+            else if (num == element2) count2++;
+        }
+
+        List<Integer> outputList = new ArrayList<>();
+        if (count1 > nums.length / 3) {
+            outputList.add(element1);
+        }
+
+        if (count2 > nums.length / 3) {
+            outputList.add(element2);
+        }
+
+        return outputList;
+    }
+}
