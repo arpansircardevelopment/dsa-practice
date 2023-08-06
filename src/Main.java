306d2244c1b07e9ba7494c0f930ad8685ae8f06d
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-08-06 08:35:32 +0530

    Linear search - added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import algorithms.LinearSearch;
 import arrays.*;
 
 import java.util.ArrayList;
@@ -5,8 +6,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        MajorityElement2 majorityElement2 = new MajorityElement2();
-        List<Integer> output = majorityElement2.majorityElement(new int[]{3, 2, 3});
-        System.out.println(output.toString());
+
     }
 }
---
+++ src/algorithms/LinearSearch.java
@@ -0,0 +1,18 @@
+package algorithms;
+
+public class LinearSearch {
+
+    // go through the array to check if the element is present or not
+    // if the element is present, return the index
+    // if the element is not present, return -1
+    public int findElement(int[] arr, int element) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == element) {
+                return i;
+            }
+        }
+
+        // if the above if block isn't executed, we will execute this statement
+        return -1;
+    }
+}
