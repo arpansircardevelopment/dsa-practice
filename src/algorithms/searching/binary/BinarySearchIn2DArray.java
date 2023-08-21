754d64566e3881f27df258e5ef5edd73b41ed512
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-08-21 06:54:17 +0530

    Binary Search In 2D Arrays - Added

---
+++ src/algorithms/searching/binary/BinarySearchIn2DArray.java
@@ -0,0 +1,22 @@
+package algorithms.searching.binary;
+
+public class BinarySearchIn2DArray {
+    public int[] search(int[][] matrix, int target) {
+        int r = 0;
+        int c = matrix.length - 1;
+
+        while (r < matrix.length && c >= 0) {
+            if (matrix[r][c] == target) {
+                return new int[]{r, c};
+            }
+
+            if (matrix[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
