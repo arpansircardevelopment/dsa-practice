b354765fecfaf50bb81135f527687f8c71038aed
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-09-14 06:46:01 +0530

    Insertion Sort - Added

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import algorithms.sorting.bubble.BubbleSort;
+import algorithms.sorting.insertion.InsertionSort;
 import algorithms.sorting.selection.SelectionSort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        SelectionSort selectionSort = new SelectionSort();
-        int[] output = selectionSort.sort(new int[]{5, 4, 3, 2, 1});
+        InsertionSort insertionSort = new InsertionSort();
+        int[] output = insertionSort.sort(new int[]{3, 4, 5, - 2, 1});
         System.out.println(Arrays.toString(output));
     }
 }
---
+++ src/algorithms/sorting/insertion/InsertionSort.java
@@ -0,0 +1,22 @@
+package algorithms.sorting.insertion;
+
+public class InsertionSort {
+    public int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
+
+    private void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
