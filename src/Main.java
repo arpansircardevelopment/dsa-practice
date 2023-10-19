b7a888f675ecacefba9b3ee6de52aa3a82d4ab74
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-10-19 07:44:59 +0530

    Power of two - added

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import algorithms.recursion.CalculatePow;
+import algorithms.recursion.PowerOfTwo;
 import algorithms.recursion.RecursiveBubbleSort;
 import algorithms.recursion.TrianglePattern2;
 
@@ -6,7 +7,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        CalculatePow pow = new CalculatePow();
-        pow.calculate(2, -5);
+        PowerOfTwo powerOfTwo = new PowerOfTwo();
+        System.out.println(powerOfTwo.isPowerOfTwo(10));
     }
 }
