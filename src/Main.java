7093ddc9a338303bdd36b36064c360546e1bf4c7
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-11-22 07:59:13 +0530

    Subset without duplicates added

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
+        int[] arr = {1, 2, 2};
         Subset subset = new Subset();
-        List<List<Integer>> output = subset.subset(arr);
+        List<List<Integer>> output = subset.subsetWithoutDuplicates(arr);
         for (List<Integer> list : output) {
             System.out.println(list);
         }
--- src/algorithms/recursion/Subset.java
+++ src/algorithms/recursion/Subset.java
@@ -1,6 +1,7 @@
 package algorithms.recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subset {
@@ -17,6 +18,31 @@ public class Subset {
             }
         }
 
+        return outer;
+    }
+
+    public List<List<Integer>> subsetWithoutDuplicates(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            start = 0;
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                start = end + 1;
+            }
+
+            end = outer.size() - 1;
+            int n = outer.size();
+
+            for (int j = start; j < n; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
         return outer;
     }
 }
