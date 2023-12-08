fdc5f85cb180ee8f8be961519028cafec97a55a3
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-12-08 07:51:25 +0530

    Path ret problem added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import algorithms.recursion.backtracking.Maze;
 import algorithms.recursion.backtracking.Path;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Path path = new Path();
-        path.path("", 3, 3);
+        ArrayList <String> arrayList = path.pathRet("", 3, 3);
+        System.out.printf(arrayList.toString());
     }
 }
--- src/algorithms/recursion/backtracking/Path.java
+++ src/algorithms/recursion/backtracking/Path.java
@@ -1,5 +1,7 @@
 package algorithms.recursion.backtracking;
 
+import java.util.ArrayList;
+
 public class Path {
     public void path(String p, int r, int c) {
         if (r == 1 && c == 1) {
@@ -14,5 +16,24 @@ public class Path {
         if (c > 1) {
             path(p + 'R', r, c - 1);
         }
+    }
+
+    public ArrayList<String> pathRet(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        if (r > 1) {
+            list.addAll(pathRet(p + 'D', r - 1, c));
+        }
+
+        if (c > 1) {
+            list.addAll(pathRet(p + 'R', r, c - 1));
+        }
+
+        return list;
     }
 }
