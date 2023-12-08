82e8b98aa1eb5505238ac8b4dea8e3340a97f8d3
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-12-08 07:31:06 +0530

    Maze problem added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import algorithms.recursion.Maze;
 import algorithms.recursion.Permutations;
 import algorithms.recursion.Subset;
 
@@ -6,8 +7,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Permutations permutations = new Permutations();
-        ArrayList<String> list = permutations.permutationsUsingIterations("", "abc");
-        System.out.println(list);
+        Maze maze = new Maze();
+        System.out.println(maze.count(3, 3));
     }
 }
---
+++ src/algorithms/recursion/Maze.java
@@ -0,0 +1,13 @@
+package algorithms.recursion;
+
+public class Maze {
+    public int count(int r, int c) {
+        if (r == 1 || c == 1) {
+            return 1;
+        }
+
+        int left = count(r - 1, c);
+        int right = count(r, c - 1);
+        return left + right;
+    }
+}
