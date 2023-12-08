e4e13211509f6cf66e9056867edf2ddde6b7f40f
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-12-08 07:57:34 +0530

    Diagonal added for path return

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Path path = new Path();
-        ArrayList <String> arrayList = path.pathRet("", 3, 3);
+        ArrayList <String> arrayList = path.pathRetDiagonal("", 3, 3);
         System.out.printf(arrayList.toString());
     }
 }
