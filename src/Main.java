c0cd33b9586ac57b8d38ef883f8c85bf522f2509
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2023-12-11 07:34:44 +0530

    Backtracking All Path Problem - Added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import algorithms.recursion.backtracking.AllPath;
 import algorithms.recursion.backtracking.Maze;
 import algorithms.recursion.backtracking.Path;
 
@@ -7,12 +8,12 @@ public class Main {
 
     static boolean[][] mazeArray = {
             {true, true, true},
-            {true, false, true},
+            {true, true, true},
             {true, true, true}
     };
 
     public static void main(String[] args) {
-        Path path = new Path();
-        path.pathRestrictions("", mazeArray, 0, 0);
+        AllPath allPath = new AllPath();
+        allPath.allPath("", mazeArray, 0, 0);
     }
 }
