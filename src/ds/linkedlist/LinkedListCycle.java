b95cc361f5a6e4144625ad319a4c3da23023d972
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-01-08 07:42:00 +0530

    Happy Number - Added

--- src/ds/linkedlist/LinkedListCycle.java
+++ src/ds/linkedlist/LinkedListCycle.java
@@ -72,6 +72,28 @@ public class LinkedListCycle {
         return s;
     }
 
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    private int findSquare(int number) {
+        int ans = 0;
+        while (number > 0) {
+            int rem = number % 10;
+            ans += rem * rem;
+            number = number / 10;
+        }
+        return ans;
+    }
+
     public boolean driver(ArrayList<Integer> elements) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
 
