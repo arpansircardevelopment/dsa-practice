9f5ffd37265c9771ada30561726bdcf83ce05a45
arpansircardevelopment <59797469+arpansircardevelopment@users.noreply.github.com>
2024-01-08 07:20:05 +0530

    Return the head of the cycle - complete

--- src/ds/linkedlist/LinkedListCycle.java
+++ src/ds/linkedlist/LinkedListCycle.java
@@ -39,6 +39,39 @@ public class LinkedListCycle {
         return 0;
     }
 
+    public SinglyLinkedList.Node detectCycle(SinglyLinkedList.Node head) {
+        int length = 0;
+
+        SinglyLinkedList.Node fast = head;
+        SinglyLinkedList.Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = lengthOfCycle(slow);
+                break;
+            }
+        }
+
+        if (length == 0) {
+            return null;
+        }
+
+        SinglyLinkedList.Node f = head;
+        SinglyLinkedList.Node s = head;
+        while (length > 0) {
+            s = s.next;
+            length--;
+        }
+
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+        return s;
+    }
+
     public boolean driver(ArrayList<Integer> elements) {
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
 
